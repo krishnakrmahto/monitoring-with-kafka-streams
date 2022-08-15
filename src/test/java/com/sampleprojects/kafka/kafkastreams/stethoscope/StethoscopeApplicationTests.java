c2894dff91e3a1a7f75868cbf778e5e84a24bf62
krishnakrmahto <krish.thorcode@gmail.com>
2022-08-15 19:42:50 +0530

    added @TestProperSource

--- src/test/java/com/sampleprojects/kafka/kafkastreams/stethoscope/StethoscopeApplicationTests.java
+++ src/test/java/com/sampleprojects/kafka/kafkastreams/stethoscope/StethoscopeApplicationTests.java
@@ -2,8 +2,10 @@ package com.sampleprojects.kafka.kafkastreams.stethoscope;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.yaml")
 class StethoscopeApplicationTests {
 
 	@Test
